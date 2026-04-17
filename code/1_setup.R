241b3cd33dda945d3a2ff97b240896bb6e76d1a2
Daniel Flanagan <dflanagan@emory.edu>
2026-04-17 15:23:14 -0400

    added explicit call to broom and broom.helpers pkgs and removed 60 game filter in 1_setup.R

--- code/1_setup.R
+++ code/1_setup.R
@@ -2,7 +2,8 @@
 here::i_am("code/1_setup.R")
 
 # -- Load packages
-pacman::p_load(dplyr, tidyr, ggplot2, zoo, gt, gtsummary)
+pacman::p_load(dplyr, tidyr, ggplot2, zoo, gt, gtsummary, broom, broom.helpers)
+
 
 # -- Load data
 data <- read.csv(here::here("data/nba_27feb2026.csv"))
@@ -10,7 +11,7 @@ data <- read.csv(here::here("data/nba_27feb2026.csv"))
 
 # -- Data cleaning
 data_clean <- data %>%
-  filter(MP >= 1000 & G >= 60) %>%
+  filter(MP >= 1000) %>%
   mutate(rank_topbottom10 = case_when(Rk %in% 1:10 ~ "Top 10",
                                       Rk %in% 196:205 ~ "Bottom 10",
                                       TRUE ~ NA))
